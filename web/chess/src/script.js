d7e06f865e31e33f462ff227a009504a88184d6d
MIKNSJ <sculetoncode@gmail.com>
2024-06-06 22:17:23 -0700

    basic board outline init

---
+++ web/chess/src/script.js
@@ -0,0 +1,29 @@
+const num_cells = 64;
+const num_rows = 8;
+const num_cols = 8;
+board = [];
+
+window.onload = function() {
+    generateCells();
+}
+
+function generateCells() {
+    // create board
+    var board = document.createElement("div");
+    board.className = "board";
+    document.body.append(board);
+
+    for (var i = 0; i < num_rows; i++) {
+        for (var j = 0; j < num_cols; j++) {
+            // <div>...</div>
+            var cell = document.createElement("div"); 
+            // <div class = "cell">...</div>
+            // or cell.classList.add
+            cell.className = "cell";
+            // <div class = "board">
+            //      <div class = "cell">...</div>
+            // </div>
+            document.getElementsByClassName("board")[0].append(cell);
+        }
+    }
+}
