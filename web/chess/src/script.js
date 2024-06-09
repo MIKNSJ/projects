181f72d7db2486a094171b018a564348f9b8c6e0
MIKNSJ <sculetoncode@gmail.com>
2024-06-08 18:07:31 -0700

    board pieces - unicode init

--- web/chess/src/script.js
+++ web/chess/src/script.js
@@ -1,6 +1,12 @@
 const num_cells = 64;
 const num_rows = 8;
 const num_cols = 8;
+white_pieces = ["&#9814", "&#9816", "&#9815", "&#9812", "&#9813", "&#9815",
+    "&#9816", "&#9814", "&#9817", "&#9817", "&#9817", "&#9817", "&#9817",
+    "&#9817", "&#9817", "&#9817"];
+black_pieces = ["&#9823", "&#9823", "&#9823", "&#9823", "&#9823", "&#9823",
+    "&#9823", "&#9823", "&#9820", "&#9822", "&#9821", "&#9818", "&#9819",
+    "&#9821", "&#9822", "&#9820"];
 board = [];
 
 window.onload = function() {
@@ -10,8 +16,9 @@ window.onload = function() {
 function generateCells() {
     // create board
     var board = document.createElement("div");
-    board.className = "board";
+    board.id = "board";
     document.body.append(board);
+    var count = 0;
 
     for (var i = 0; i < num_rows; i++) {
         for (var j = 0; j < num_cols; j++) {
@@ -31,10 +38,24 @@ function generateCells() {
                 cell.style.backgroundColor = "#F5F5DC";
             }
 
-            // <div class = "board">
+            // <div id = "board">
             //      <div class = "cell">...</div>
             // </div>
-            document.getElementsByClassName("board")[0].append(cell);
+            document.getElementById("board").append(cell);
+
+            if (i < 2 || i > 5) {
+                var piece = document.createElement("div");
+                piece.id = "pieces";
+                if (i < 2) {
+                    piece.innerHTML = black_pieces.pop();
+                }
+
+                if (i > 5) {
+                    piece.innerHTML = white_pieces.pop();
+                }
+                document.getElementsByClassName("cell")[count].append(piece);
+            }
+            ++count;
         }
     }
 }
