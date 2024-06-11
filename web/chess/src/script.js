cfbfa376969a6cbce47be73f914b6ab815a2884f
MIKNSJ <sculetoncode@gmail.com>
2024-06-11 01:37:50 -0700

    basic drag and drop func completed

--- web/chess/src/script.js
+++ web/chess/src/script.js
@@ -9,17 +9,26 @@ black_pieces = ["&#9823", "&#9823", "&#9823", "&#9823", "&#9823", "&#9823",
     "&#9821", "&#9822", "&#9820"];
 board = [];
 
+
+
 window.onload = function() {
     generateCells();
 }
 
-function allowDrop(e) {
+function dragOver(e) {
     e.preventDefault();
 }
 
 function drag(e) {
-    console.log(e.target);
     e.dataTransfer.setData("text", e.target.id);
+    var drag_image = e.target.cloneNode(true);
+    drag_image.style.position = "absolute";
+    drag_image.style.left = "-99999px";
+    drag_image.style.top = "-99999px";
+    drag_image.id = "copy";
+    drag_image.innerHTML = e.target.innerHTML;
+    document.body.appendChild(drag_image);
+    e.dataTransfer.setDragImage(drag_image, 35, 40);
 }
 
 function drop(e) {
@@ -28,7 +37,11 @@ function drop(e) {
     var selected_piece = document.getElementById(data);
 
     if (e.target.className == "cell") {
-        e.target.append(selected_piece);
+        e.target.appendChild(selected_piece);
+        var drag_image = document.getElementById("copy");
+        if (drag_image) {
+            drag_image.parentNode.removeChild(drag_image);
+        }
     }
 }
 
@@ -46,8 +59,8 @@ function generateCells() {
             // <div class = "cell">...</div>
             // or cell.classList.add
             cell.className = "cell";
-            cell.ondrop = drop;
-            cell.ondragover = allowDrop;
+            cell.addEventListener("drop", drop);
+            cell.addEventListener("dragover", dragOver);
 
             if (i % 2 == 0 && j % 2 == 0) {
                 cell.style.backgroundColor = "#F5F5DC";
@@ -62,24 +75,26 @@ function generateCells() {
             // <div id = "board">
             //      <div class = "cell">...</div>
             // </div>
-            document.getElementById("board").append(cell);
+            board.append(cell);
+            //document.getElementById("board").append(cell);
 
             if (i < 2 || i > 5) {
                 var piece = document.createElement("div");
                 piece.className = "pieces";
                 piece.draggable = "true";
-                piece.ondragstart = drag;
-                piece.id = i + "-" + j;
+                piece.addEventListener("dragstart", drag);
+                piece.id = count;
 
-                if (i < 2) {
+                if (i < 2 && black_pieces.length > 0) {
                     piece.innerHTML = black_pieces.pop();
                 }
 
-                if (i > 5) {
+                if (i > 5 && white_pieces.length > 0) {
                     piece.innerHTML = white_pieces.pop();
                 }
 
-                document.getElementsByClassName("cell")[count].append(piece);
+                //document.getElementsByClassName("cell")[count].append(piece);
+                cell.append(piece);
             }
             
             ++count;
